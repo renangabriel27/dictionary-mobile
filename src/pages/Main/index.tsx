1be25bf4630265f39cb13be4b73245906cd8fda9
Renan Gabriel <renanthompsom@gmail.com>
2020-07-25 13:20:19 -0300

    Configure eslint, prettier and tsconfig

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Main: React.FC = () => {
+  return (
+    <View>
+      <Text>Seach</Text>
+    </View>
+  );
+};
+
+export default Main;
